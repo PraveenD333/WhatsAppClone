4c806415143dc1e29d765fe9912b1bf911318d7b
Praveen <praveendhane97@gmail.com>
2026-03-08 13:59:21 +0530

    Update emai.serv.js

--- Backend/Services/emai.serv.js
+++ Backend/Services/emai.serv.js
@@ -3,7 +3,10 @@ dotenv.config({quiet:true})
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    // service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
